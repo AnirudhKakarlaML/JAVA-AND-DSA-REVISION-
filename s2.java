2d7a0ff90fa13a734e0f39bec7a0568747de9481
AnirudhKakarlaML <kakarlaanirudh85@gmail.com>
2026-06-15 15:16:57 +0530

    Add files via upload

---
+++ s2.java
@@ -0,0 +1,65 @@
+/*public class s2 {
+    public static void greet(){
+        System.out.println("Hi Wolrd");
+    }
+    public static void main(String[] args){
+        greet();
+        greet();
+        greet();
+    }
+    
+}*/
+
+
+
+/*public class s2{
+    public static void PrintName(String name){
+        System.out.println(name);
+    }
+    public static void main(String [] args){
+        PrintName("Anirudh");
+        PrintName("Yash");
+    }
+}*/
+
+/*public class s2{
+    public static int add(int a , int b){
+        return a+b;
+    }
+    public static void main(String[] args){
+       int x =  add(12, 13);
+       System.out.println(x);
+
+    }
+}*/
+
+/*public class s2{
+    public static int product(int a , int b){
+        return a*b;
+    }
+    public static void main(String[] args) {
+        int x = product(1, 2);
+        System.out.println(x);
+    }
+}*/
+
+/*public class s2{
+    public static int factorial(int n){
+        int fact = 1;
+        for(int i = 1 ; i<=n ; i++){
+            fact*=i;
+        }
+        return fact;
+    }
+    public static void main(String[] args) {
+        int x = factorial(5);
+        System.out.println(x);
+    }
+}*/
+
+//Binomial Coefficients
+public class s2{
+    public static int factorial{
+        
+    }
+}
