6fe7db78b1167372e3e420010953a825da60c372
AnirudhKakarlaML <kakarlaanirudh85@gmail.com>
2026-08-05 22:06:06 +0530

    Add files via upload

---
+++ lc2574.java
@@ -0,0 +1,25 @@
+/*public class lc2574 {
+    class Solution {
+    public int[] leftRightDifference(int[] nums) {
+        int left[] = new int[nums.length];
+        left[0] = 0;
+        for(int i = 1 ; i < nums.length ; i++){
+            left[i] = left[i-1]+nums[i-1];
+        }
+        int right[] = new int[nums.length];
+        right[nums.length-1] = 0;
+        for(int i = nums.length - 2 ; i >= 0 ; i--){
+            right[i] = right[i+1]+nums[i+1];
+            
+        }
+        int result[] = new int[nums.length];
+        for(int i = 0 ; i < nums.length ; i++){
+            result[i] = Math.abs(left[i] - right[i]);
+        }
+        return result;
+    }
+}
+    
+
+}*/
+
