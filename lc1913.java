ee59c0557364c0e498afeb8dcfadf348d2efbf14
AnirudhKakarlaML <kakarlaanirudh85@gmail.com>
2026-08-10 19:06:33 +0530

    Add files via upload

---
+++ lc1913.java
@@ -0,0 +1,14 @@
+/*import java.util.Arrays;
+
+public class lc1913 {
+    class Solution {
+    public int maxProductDifference(int[] nums) {
+        Arrays.sort(nums);
+        int prod_last = nums[nums.length-1]*nums[nums.length-2];
+        int prod_first = nums[0]*nums[1];
+        return(prod_last - prod_first);
+    }
+}
+    
+}*/
+
--- lc1991.java
+++ lc1991.java
@@ -1,3 +1,26 @@
-public class lc1991 {
-    
+/*public class lc1991 {
+    class Solution {
+    public int findMiddleIndex(int[] nums) {
+        int left[] = new int[nums.length];
+        left[0] = 0;
+        for(int i = 1 ; i < nums.length ; i++){
+            left[i] = left[i-1]+nums[i-1];
+        } 
+        int right[] = new int[nums.length];
+        right[nums.length-1] = 0 ;
+        for(int i = nums.length - 2 ; i >= 0 ; i--){
+            right[i] = right[i+1]+nums[i+1];
+        }
+        
+        for(int i = 0 ; i < nums.length ; i++){
+            if(left[i] == right[i]){
+                return i;
+            }
+            
+        }
+        return -1;
+
+
+    }
 }
+}*/
