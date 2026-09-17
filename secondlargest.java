88e7f92fdf0f8c159ead5dc75fb4c738f2c4cb57
AnirudhKakarlaML <kakarlaanirudh85@gmail.com>
2026-09-17 11:19:09 +0530

    Add files via upload

---
+++ secondlargest.java
@@ -0,0 +1,18 @@
+public class secondlargest{
+    public static void main(String[]args){
+        int[]arr ={1,2,3,4,5};
+        int MAX = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+        for(int i = 0 ; i<arr.length ; i++){
+            if(arr[i]>MAX){
+                MAX = arr[i];
+            }
+        }
+        for(int i = 0 ; i < arr.length ; i++){
+            if(arr[i]<MAX && arr[i]>max2){
+                max2 = arr[i];
+            }
+        }
+        System.out.println(max2);
+    }
+}
