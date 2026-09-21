0f46d93d56a3e0a315b5e118ede50d9e3889e05a
AnirudhKakarlaML <kakarlaanirudh85@gmail.com>
2026-09-21 21:36:21 +0530

    Add files via upload

---
+++ lc1672.java
@@ -0,0 +1,22 @@
+public class lc1672 {
+    class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int wealth = 0;
+        int m = accounts.length;
+        int n = accounts[0].length;
+        for(int i = 0 ; i < m ; i++){
+            int currsum = 0;
+            for(int j = 0 ; j < n ; j++){
+                
+                currsum+=accounts[i][j];
+                
+            }
+            if(currsum>wealth){
+                wealth = currsum;
+            }
+            
+        }
+        return wealth;
+    }
+}
+}
