cf0cee0a45373dc50ef06a95edc8e7faf3057237
AnirudhKakarlaML <kakarlaanirudh85@gmail.com>
2026-09-25 17:38:26 +0530

    Add files via upload

---
+++ lc485.java
@@ -0,0 +1,21 @@
+public class lc485 {
+    class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int count = 0;
+        int maxOnes = 0;
+        for(int i = 0 ; i < nums.length ; i++){
+            if(nums[i]>0){
+                count++;
+                if(count>maxOnes){
+                    maxOnes = count;
+                }
+            }
+            else{
+                count = 0;
+            }
+        }
+        return maxOnes;
+    }
+
+}
+}
